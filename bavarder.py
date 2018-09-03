cb284b08315dbe8854ba1ec506cd551b185dea99
ethan <alegosdude@gmail.com>
2018-09-02 23:00:58 -0400

    new font, more layout

--- bavarder.py
+++ bavarder.py
@@ -6,9 +6,31 @@ urls=(
 
 app=web.application(urls, globals())
 render=web.template.render("templates/", base="layout")
+users = [
+    "Sample Human 1",
+    "Sample Human 2",
+    "Sample Human 3",
+    "Sample Human 4",
+    "Sample Human 5",
+    "Sample Human 6",
+    "Sample Human 7",
+    "Sample Human 8",
+    "Sample Human 9",
+    "Sample Human 10",
+    "Sample Human 11",
+    "Sample Human 12",
+    "Sample Human 13",
+    "Sample Human 14",
+    "Sample Human 15",
+    "Sample Human 16",
+    "Sample Human 17",
+    "Sample Human 18",
+    "Sample Human 19",
+    "Sample Human 20"
+]
 
 class index:
     def GET(self):
-        return render.index()
+        return render.index(users)
 
 app=app.gaerun()
--- scripts/bavarder.js
+++ scripts/bavarder.js
@@ -12,7 +12,21 @@ if ('serviceWorker' in navigator) {
 function add_chat_action(){
     document.getElementsByClassName("add_chat_screen")[0].style.bottom="0%";
 }
-function back_chat_action(){
+function back_add_chat_action(){
     document.getElementsByClassName("add_chat_screen")[0].style.bottom="-100%";
     document.getElementById("unameip").value="";
+}
+function chat_action(name){
+    document.getElementById("user").innerHTML=name;
+    document.getElementsByClassName("chat_screen")[0].style.right="0%";
+}
+function back_chat_action(){
+    document.getElementsByClassName("chat_screen")[0].style.right="-100%";
+}
+function osc(x,y){
+    if (document.getElementsByClassName("scroll")[x].scrollTop==0) {
+        document.getElementsByClassName("hd")[y].style.boxShadow="#000000 0 0 0"
+    } else {
+        document.getElementsByClassName("hd")[y].style.boxShadow="#00000061 0 2px 4px"
+    }
 }
