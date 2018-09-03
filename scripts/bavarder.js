daf7101fd1db051a3883a3ae98d6f0a186ea1df2
ethan <alegosdude@gmail.com>
2018-09-02 21:13:57 -0400

    pwa

--- scripts/bavarder.js
+++ scripts/bavarder.js
@@ -1,3 +1,14 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js', {updateViaCache: 'none'}).then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+  
+
 function add_chat_action(){
     document.getElementsByClassName("add_chat_screen")[0].style.bottom="0%";
 }
---
+++ scripts/sw.js
@@ -0,0 +1,44 @@
+var CACHE_NAME = 'bavardercsh-2';
+var urlsToCache = [
+  '/',
+  '/material.css',
+  '/bavarder.js'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        Promise.all([caches.open(CACHE_NAME), self.skipWaiting()])
+            .then(function(cache) {
+                console.log('Opened Cache');
+                return cache[0].addAll(urlsToCache)
+            })
+    )
+})
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
+    );
+  });
+
+self.addEventListener('activate', function(event) {
+
+    var cacheWhitelist = ['bavardercsh'];
+
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+        return Promise.all(
+            cacheNames.map(function(cacheName) {
+                return caches.delete(cacheName);
+            })
+        );
+        })
+    );
+});
