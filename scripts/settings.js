01e16450cdc86e92f7ce5ee33b1bbede94d78cb5
storm246 <alegosdude@gmail.com>
2018-10-14 21:52:13 -0400

    fixed colors on bottom app bar

--- scripts/settings.js
+++ scripts/settings.js
@@ -39,7 +39,7 @@ function toggleamoleddark(){
     for (var i = 0; i<=document.getElementsByClassName("card").length-1; i++){ document.getElementsByClassName("card")[i].style.background="black"; }
     for (var i = 0; i<=document.getElementsByClassName("chatbutton").length-1; i++){ document.getElementsByClassName("chatbutton")[i].style.background="black"; }
     for (var i = 0; i<=document.getElementsByClassName("hdiconbtn").length-1; i++){ 
-        document.getElementsByClassName("hdiconbtn")[i].style.background="#0d0d0d";
+        document.getElementsByClassName("hdiconbtn")[i].style.background="black";
         document.getElementsByClassName("hdiconbtn")[i].style.color="white"; 
     }
     for (var i = 0; i<=document.getElementsByClassName("action").length-1; i++){ 
